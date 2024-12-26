dc083cd056fdc12ae63a4eb43cb3244cc7758e9c
irenecats <37797113+irenecats@users.noreply.github.com>
2024-12-27 02:08:55 +0900

    Fix animation on refresh
    Add the reverse animation using react logic so it is only reproduced on mouse leave

--- src/app/components/project-elem.tsx
+++ src/app/components/project-elem.tsx
@@ -1,9 +1,28 @@
+"use client";
 import Image from "next/image";
 import Styles from "./project-elem.module.css";
+import { useState } from "react";
 
 export default function ProjectElem() {
+  const [animationClass, setClass] = useState(false);
+  const reverseAnimation = animationClass ? `${Styles["project-bg-rev"]}` : "";
+
+  function removeHoverListener(e: React.MouseEvent): void {
+    e.target.removeEventListener("mouseleave", () => {
+      setClass(false);
+    });
+  }
+
+  function addHoverListener(e: React.MouseEvent): void {
+    e.target.addEventListener("mouseleave", () => {
+      setClass(true);
+    });
+  }
+
   return (
     <div
+      onMouseEnter={addHoverListener}
+      onMouseLeave={removeHoverListener}
       className={`${Styles.wrapper} max-w-[22%] relative h-fit border-8 border-[#243c71] rounded`}
     >
       <Image
@@ -14,7 +33,7 @@ export default function ProjectElem() {
         alt="test img"
       />
       <div
-        className={`${Styles["project-bg"]} absolute top-8 left-0 w-3/4 py-1 px-4 bg-[#243c71] pl-5 rounded-r`}
+        className={`${Styles["project-bg"]} ${reverseAnimation} absolute top-8 left-0 w-3/4 py-1 px-4 bg-[#243c71] pl-5 rounded-r`}
       >
         <h1
           className={`${Styles["project-title"]} tracking-wider text-3xl text-[#8375d] whitespace-nowrap overflow-hidden font-semibold`}
