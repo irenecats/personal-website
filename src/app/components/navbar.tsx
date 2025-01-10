6b32134b6b00e6b508c0d817cf83359f759b968e
irenecats <37797113+irenecats@users.noreply.github.com>
2025-01-10 16:20:04 +0900

    Fix navbar key error

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { Children, ReactNode } from "react";
 import Separator, { SeparatorTypeEnum } from "./nav-separator";
 
 interface NavBarProps {
@@ -9,23 +9,16 @@ export default function NavBar({ children }: NavBarProps) {
   return (
     <header>
       <nav className="flex justify-end gap-10 m-0 mt-6 px-5 absolute right-0 left-0 z-10 font-semibold text-[#dadfe2]">
-        {children.map((elem: ReactNode, index: number) => {
+        {Children.map(children, (child: ReactNode, index: number) => {
           const useSeparator = index > 1;
           const elemStyle = index == 0 ? "mr-auto" : "";
           return useSeparator ? (
             <>
-              <Separator
-                key={index + "separator"}
-                separatorType={SeparatorTypeEnum.SparkleCustom}
-              />
-              <div key={index} className="navLink">
-                {elem}
-              </div>
+              <Separator separatorType={SeparatorTypeEnum.SparkleCustom} />
+              <div className="navLink">{child}</div>
             </>
           ) : (
-            <div key={index} className={`${elemStyle} navLink`}>
-              {elem}
-            </div>
+            <div className={`${elemStyle} navLink`}>{child}</div>
           );
         })}
       </nav>
