4e48df59dabf1eaf9ea8d006d82146501809a989
irenecats <37797113+irenecats@users.noreply.github.com>
2025-02-08 01:04:00 +0900

    Add stars to footer and fix resizing issues

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,4 @@
+import DecorativeFooter from "./decoration/decorative-footer";
 import SocialLink, { SocialEnum } from "./social-link";
 
 interface Props {
@@ -6,13 +7,16 @@ interface Props {
 
 export default function Footer({ idElement }: Props) {
   return (
-    <footer
-      id={idElement}
-      className="relative flex flex-row justify-center gap-6 snap-end py-6 md:py-10 bg-[#dadfe2] z-10"
-    >
-      <SocialLink social={SocialEnum.EMAIL} className="social" />
-      <SocialLink social={SocialEnum.LINKEDIN} className="social" />
-      <SocialLink social={SocialEnum.GITHUB} className="social" />
-    </footer>
+    <div className="relative pointer-events-none z-10 overflow-clip">
+      <DecorativeFooter />
+      <footer
+        id={idElement}
+        className="flex flex-row justify-center gap-6 snap-end py-6 md:py-10 bg-[#dadfe2] "
+      >
+        <SocialLink social={SocialEnum.EMAIL} className="social" />
+        <SocialLink social={SocialEnum.LINKEDIN} className="social" />
+        <SocialLink social={SocialEnum.GITHUB} className="social" />
+      </footer>
+    </div>
   );
 }
