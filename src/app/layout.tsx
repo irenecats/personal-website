f622ac395c5655d51d6f0512d024bc0b07ff7612
irenecats <37797113+irenecats@users.noreply.github.com>
2025-02-08 17:47:50 +0900

    static page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,7 @@ export const metadata: Metadata = {
   title: "Irene Clemente's portfolio",
   description: "Generated with Next js",
 };
-import NavBar from "./components/navbar";
-import NavLink from "./components/nav-link";
-import "./ui/global.css";
-import Footer from "./components/footer";
+import "./global.css";
 
 export default function RootLayout({
   children,
@@ -19,13 +16,7 @@ export default function RootLayout({
       <body
         className={`antialiased bg-[#071a45] text-[#dadfe2] overflow-x-hidden`}
       >
-        <NavBar>
-          <NavLink title="about" href="#about" />
-          <NavLink title="projects" href="#projects" />
-          <NavLink title="contact" href="#contact" />
-        </NavBar>
         <main className={`px-4 relative pointer-events-none`}>{children}</main>
-        <Footer idElement="contact" />
       </body>
     </html>
   );
