808cad02ac3111c7f325be82a240a44cc590f00c
irenecats <37797113+irenecats@users.noreply.github.com>
2025-02-08 18:09:11 +0900

    Add fonts back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,22 @@ export const metadata: Metadata = {
   title: "Irene Clemente's portfolio",
   description: "Generated with Next js",
 };
+import { Inter } from "next/font/google";
 import NavBar from "./components/navbar";
 import NavLink from "./components/nav-link";
 import "./ui/global.css";
 import Footer from "./components/footer";
-
+const roboto = Inter({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`font-mono`}>
+    <html lang="en" className={`${roboto.variable} font-mono`}>
       <body
         className={`antialiased bg-[#071a45] text-[#dadfe2] overflow-x-hidden`}
       >
